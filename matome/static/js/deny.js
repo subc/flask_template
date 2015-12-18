3b86f6d321e41990c466caac3654dd0f3ca8f079
ikeda <ikeda@000196-M3.local>
2015-12-18 15:17:59 +0900

    コピー禁止機能とGoogle Analytics対応

---
+++ matome/static/js/deny.js
@@ -0,0 +1,44 @@
+// ホストネームに含まれる文字列
+host_name = 'niku.tokyo';
+
+// コピーされたときのredirect先
+// redirect_deny_url = 'http://www.niku.tokyo/';
+redirect_millisec = 3000;
+
+// コピーされたときに表示するメッセージ
+deny_message = '<h3 style="color: white">本ページは削除されました。(・ω<) ﾃﾍﾍﾟﾛ </h3>';
+redirect_deny_url = $('meta[name=Identifier-URL]').attr("content");
+
+
+function deny(){
+    // ローカルなら動かさない
+    if ($(location).attr('hostname') == '' || $(location).attr('hostname') == '127.0.0.1'){
+        console.info('is local');
+        return null;
+    }
+
+    // ホストが異なるなら実行
+    if ($(location).attr('hostname').match(host_name)){
+        // 正常
+        console.info('my site');
+        return null;
+    }else{
+        // コピーサイト
+        console.info('copy site');
+        disable();
+    }
+}
+
+function disable(){
+    console.info('start disable');
+    // div.mainを書き換え
+    $("div.container").html(deny_message);
+    // N秒後にredirect
+    setTimeout(function(){
+        $(document).ready( function() {
+            $(location).attr("href", redirect_deny_url);
+        });
+    },redirect_millisec);
+}
+
+$(deny);

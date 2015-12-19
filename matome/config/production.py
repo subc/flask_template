eb63915eb2b7d5d8355077767d703d74ff25eecc
conoha <hoge>
2015-12-19 16:17:45 +0900

    設定ファイル上書き

---
+++ matome/config/production.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from config.base import *
+
+# database
+DB = {
+    'db_name': 'matome',
+    'user': 'root',
+    'password': '',
+    'host': '192.168.0.2',
+    'port': 3306
+}
+
+
+
--- matome/scripts/production_server.sh
+++ matome/scripts/production_server.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-GUNICORN=$HOME/.virtualenvs/matome/bin/gunicorn
+GUNICORN=/root/.virtualenvs/matome/bin/gunicorn
 PROJECT_ROOT=/var/flask/matome/matome
 
 APP=wsgi:app
 
 cd $PROJECT_ROOT
-exec $GUNICORN -c $PROJECT_ROOT/config/gunicorn/gunicorn.conf.py $APP
+exec $GUNICORN -c $PROJECT_ROOT/config/gunicorn/guniconf.py $APP

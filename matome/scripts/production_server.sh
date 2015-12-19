c11c9f723a2b732208c7a4df5bb1f56c86c492f9
conoha <hoge>
2015-12-19 11:53:44 +0900

    update settings

--- matome/scripts/production_server.sh
+++ matome/scripts/production_server.sh
@@ -6,4 +6,4 @@ PROJECT_ROOT=/var/flask/matome/matome
 APP=wsgi:app
 
 cd $PROJECT_ROOT
-exec $GUNICORN -c $PROJECT_ROOT/gunicorn.conf.py $APP
+exec $GUNICORN -c $PROJECT_ROOT/config/gunicorn/gunicorn.conf.py $APP

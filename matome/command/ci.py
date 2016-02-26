ef19de5ea8a0d8905d6bd9fa9146249eabcc96db
ikeda <ikeda@000196-M3.local>
2016-02-26 17:30:07 +0900

    テスト書いた

--- matome/command/ci.py
+++ matome/command/ci.py
@@ -1,11 +1,28 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, unicode_literals
+
+import requests
 from flask_script import Command
 
 
 class CI(Command):
     """
-    Circle CI用
+    Circle CIの試験用
     """
     def run(self):
-        print('HelloWorld!')
+        test_urls = [
+            "http://127.0.0.1:5000/example/",
+        ]
+
+        for url in test_urls:
+            get_test(url)
+            print("OK!:{}".format(url))
+
+
+def get_test(url):
+    headers = {
+        "content-type": "text/html; charset=UTF-8"
+    }
+    response = requests.get(url, headers=headers)
+    assert response.status_code == 200
+    assert "Error" not in response.text

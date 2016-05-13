afadf4b55c9cdcdb0c7844c6b6e7e94e9ee19ca9
ikeda <ikeda@000196-M3.local>
2016-05-13 18:52:28 +0900

    extend overwatch data

--- matome/module/scraping/search.py
+++ matome/module/scraping/search.py
@@ -327,6 +327,21 @@ class SearchManager(object):
         keywords_ignore += ignore_base
         return _base_search(subjects, site, keywords, keywords_ignore)
 
+    def overwatch(self, subjects, site):
+        """
+        :param subjects:
+        :param site:
+        :return:
+        """
+        keywords = [
+            'Overwatch',
+            'オーバーウォッチ',
+        ]
+        keywords_ignore = [
+        ]
+        keywords_ignore += ignore_base
+        return _base_search(subjects, site, keywords, keywords_ignore)
+
 
 def _base_search(subjects, site, keywords, keywords_ignore):
     """

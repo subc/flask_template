8d170990f485854f51fe32570c2b1d610297795b
ikeda <ikeda@ikedashuuhei-no-iMac.local>
2016-07-18 12:24:32 +0900

    update deploy script

--- matome/scripts/deploy.sh
+++ matome/scripts/deploy.sh
@@ -3,7 +3,7 @@
 set -eu
 
 echo "conoha2 deploy start"
-/Users/ikeda/.virtualenvs/py35/bin/py.test /Users/ikeda/punk/matome/matome/tests/tests_deploy.py
+~/.virtualenvs/matome/bin/py.test ~/python/matome/matome/tests/tests_deploy.py
 ssh -l root conoha2 "date"
 ssh -l root conoha2 "cd /var/flask/matome/matome && git pull origin master"
 ssh -l root conoha2 "cd /var/flask/matome/matome && /root/.virtualenvs/matome/bin/python manage.py -c ./config/production.py ins"
@@ -17,7 +17,7 @@ echo "~~~~~~~~~~~~"
 echo "HTTP STATUS CHECK"
 echo "~~~~~~~~~~~~"
 ab -n 300 -c 10 http://www.niku.tokyo/fallout4/
-/Users/ikeda/.virtualenvs/py35/bin/py.test /Users/ikeda/punk/matome/matome/tests/tests_deploy.py
+~/.virtualenvs/matome/bin/py.test ~/python/matome/matome/tests/tests_deploy.py
 curl -LI http://www.niku.tokyo/fallout4/ -o /dev/null -w '%{http_code}\n' -s
 curl -LI http://www.niku.tokyo/fallout4pc/ -o /dev/null -w '%{http_code}\n' -s
 curl -LI http://www.niku.tokyo/phantom/ -o /dev/null -w '%{http_code}\n' -s
